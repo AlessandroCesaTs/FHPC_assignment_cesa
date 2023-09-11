ed436f251c0c2597fe2388cd9cd315353401fe0e
AlessandroCesaTs <alessandro.cesa22@gmail.com>
2023-09-11 16:00:00 +0200

    fixed errors in main and deleted debig statements

--- exercise1/source/main.c
+++ exercise1/source/main.c
@@ -15,20 +15,13 @@ int main(int argc,char** argv){
     char* char_image;
     read_pgm_image(&image,&maxval, &xsize, &ysize, "test.pgm");
     char_image=(char*)image;
-    ysize=0;
-    maxval=0;
-    free(image);
-    char_image=(char*)image;
     for (int t=0;t<101;t++){
          run_episode_ordered(&char_image,xsize);
-	 printf("episode runned \n");
-	 fflush(stdout);
-         if (t % 20 == 0) {
+         if (t %100 == 0) {
                  char new_image_name[20];  // Adjust the array size as needed
                 snprintf(new_image_name, sizeof(new_image_name), "test_%d.pgm", t);
                 write_pgm_image((void*)char_image, xsize, new_image_name);
                 }
         }
     return 0;
-
 }
--- exercise1/source/run.c
+++ exercise1/source/run.c
@@ -4,47 +4,7 @@
 #include"run.h"
 #include "image_handling.h"
 
-
-void upgrade_cell(char** grid,int size,int i,int j){
-    char* g=*grid;
-    printf("call evaluate cell \n");
-    fflush(stdout);
-    g[i*size+j]=evaluate_cell(grid,size,i,j);
-}
-
-
-void run_episode_ordered(char** grid,int size){
-	printf("start running episode\n");
-	fflush(stdout);
-    for (int i=0;i<size;i++){
-        for (int j=0;j<size;j++){
-		printf("call upgrade_cell \n");
-		fflush(stdout);
-            upgrade_cell(grid,size,i,j);
-        }
-    }
-}
-
-void run_episode_static(char** grid,int size){
-    char **eval = (char **)malloc(size * sizeof(char *));
-    for (int i=0;i<size;i++){
-        eval[i]=(char*)malloc(size * sizeof(char));
-        for (int j=0;j<size;j++){
-            eval[i][j]=evaluate_cell(grid,size,i,j);
-        }
-    }
-char* g=*grid;
-    for (int i=0;i<size;i++){
-            for (int j=0;j<size;j++){
-           g[i*size+j]=eval[i][j];
-            }
-    }
-    free(eval);
-}
-
 char evaluate_cell(char** grid,int size,int i,int j){
-    printf("evaluate cell called\n");
-    fflush(stdout);
     int u_i,d_i,l_j,r_j;
     int neighborhood;
     char* g=*grid;
@@ -69,13 +29,9 @@ char evaluate_cell(char** grid,int size,int i,int j){
         l_j=j-1;
         r_j=j+1;
     }
-    printf("compute neighborhhod\n ");
-    fflush(stdout);
     neighborhood=(int)g[u_i*size+l_j]+(int)g[u_i*size+j]+
 	    (int)g[u_i*size+r_j]+(int)g[i*size+l_j]+
 	    (int)g[i*size+r_j]+(int)g[d_i*size+l_j]+(int)g[d_i*size+j]+(int)g[d_i*size+r_j];
-    printf("neighborhood computed \n");
-    fflush(stdout);
  if (neighborhood==2 || neighborhood==3){
             return (char)1;
         }else{
@@ -83,3 +39,33 @@ char evaluate_cell(char** grid,int size,int i,int j){
         }
 }
 
+void update_cell(char** grid,int size,int i,int j){
+    char* g=*grid;
+    g[i*size+j]=evaluate_cell(grid,size,i,j);
+}
+
+
+void run_episode_ordered(char** grid,int size){
+    for (int i=0;i<size;i++){
+        for (int j=0;j<size;j++){
+            update_cell(grid,size,i,j);
+        }
+    }
+}
+
+void run_episode_static(char** grid,int size){
+    char **eval = (char **)malloc(size * sizeof(char *));
+    for (int i=0;i<size;i++){
+        eval[i]=(char*)malloc(size * sizeof(char));
+        for (int j=0;j<size;j++){
+            eval[i][j]=evaluate_cell(grid,size,i,j);
+        }
+    }
+char* g=*grid;
+    for (int i=0;i<size;i++){
+            for (int j=0;j<size;j++){
+           g[i*size+j]=eval[i][j];
+            }
+    }
+    free(eval);
+}
